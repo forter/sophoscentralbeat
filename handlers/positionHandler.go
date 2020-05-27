339893f69d88f435164e63d430b3db06ca2cee53
Abhishek Upadhyay <Abhishek.Upadhyay@logrhythm.com>
2020-05-26 23:02:05 -0700

    removing pos file logging

--- handlers/positionHandler.go
+++ handlers/positionHandler.go
@@ -81,8 +81,6 @@ func GetPostionFile() (string, error) {
 
 	posFolder := filepath.Join(tokenCacheDir, "logs")
 
-	logp.Info("Position folder %v", posFolder)
-
 	err := os.MkdirAll(posFolder, 0700)
 	if err != nil {
 		return "", err
